649d83439255436d7ac6370dbea690b843a0713c
sebif <sebif@DESKTOP-KV377TE>
2022-02-05 11:12:53 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace infoEducatie
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Iesire = new System.Windows.Forms.Button();
+            this.butonX = new System.Windows.Forms.Button();
+            this.butonMaximizare = new System.Windows.Forms.Button();
+            this.butonMinimizare = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.butonAjutor = new System.Windows.Forms.Button();
+            this.butonOptiuni = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Iesire
+            // 
+            this.Iesire.AccessibleRole = System.Windows.Forms.AccessibleRole.ScrollBar;
+            this.Iesire.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.Iesire.AutoSize = true;
+            this.Iesire.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(49)))), ((int)(((byte)(49)))));
+            this.Iesire.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.Iesire.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Iesire.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Iesire.ForeColor = System.Drawing.Color.LightSeaGreen;
+            this.Iesire.Location = new System.Drawing.Point(302, 384);
+            this.Iesire.Margin = new System.Windows.Forms.Padding(0);
+            this.Iesire.Name = "Iesire";
+            this.Iesire.Size = new System.Drawing.Size(202, 38);
+            this.Iesire.TabIndex = 0;
+            this.Iesire.Text = "Iesire";
+            this.Iesire.UseVisualStyleBackColor = false;
+            this.Iesire.Click += new System.EventHandler(this.Iesire_Click);
+            // 
+            // butonX
+            // 
+            this.butonX.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.butonX.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(49)))), ((int)(((byte)(49)))));
+            this.butonX.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.butonX.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.butonX.ForeColor = System.Drawing.Color.LightSeaGreen;
+            this.butonX.Location = new System.Drawing.Point(727, 0);
+            this.butonX.Name = "butonX";
+            this.butonX.Size = new System.Drawing.Size(73, 22);
+            this.butonX.TabIndex = 1;
+            this.butonX.Text = "X";
+            this.butonX.UseMnemonic = false;
+            this.butonX.UseVisualStyleBackColor = false;
+            this.butonX.Click += new System.EventHandler(this.butonX_Click);
+            // 
+            // butonMaximizare
+            // 
+            this.butonMaximizare.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.butonMaximizare.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(49)))), ((int)(((byte)(49)))));
+            this.butonMaximizare.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.butonMaximizare.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.butonMaximizare.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.butonMaximizare.ForeColor = System.Drawing.Color.LightSeaGreen;
+            this.butonMaximizare.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.butonMaximizare.Location = new System.Drawing.Point(655, 0);
+            this.butonMaximizare.Name = "butonMaximizare";
+            this.butonMaximizare.Size = new System.Drawing.Size(73, 22);
+            this.butonMaximizare.TabIndex = 2;
+            this.butonMaximizare.Text = "⬜";
+            this.butonMaximizare.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.butonMaximizare.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.butonMaximizare.UseMnemonic = false;
+            this.butonMaximizare.UseVisualStyleBackColor = false;
+            this.butonMaximizare.Click += new System.EventHandler(this.butonMaximizare_Click);
+            // 
+            // butonMinimizare
+            // 
+            this.butonMinimizare.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.butonMinimizare.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(49)))), ((int)(((byte)(49)))));
+            this.butonMinimizare.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.butonMinimizare.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.butonMinimizare.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.butonMinimizare.ForeColor = System.Drawing.Color.LightSeaGreen;
+            this.butonMinimizare.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.butonMinimizare.Location = new System.Drawing.Point(582, 0);
+            this.butonMinimizare.Name = "butonMinimizare";
+            this.butonMinimizare.Size = new System.Drawing.Size(73, 22);
+            this.butonMinimizare.TabIndex = 3;
+            this.butonMinimizare.Text = "_";
+            this.butonMinimizare.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.butonMinimizare.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.butonMinimizare.UseMnemonic = false;
+            this.butonMinimizare.UseVisualStyleBackColor = false;
+            this.butonMinimizare.Click += new System.EventHandler(this.butonMinimizare_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(49)))), ((int)(((byte)(49)))));
+            this.button4.FlatAppearance.BorderColor = System.Drawing.Color.LightSeaGreen;
+            this.button4.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(49)))), ((int)(((byte)(49)))));
+            this.button4.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(49)))), ((int)(((byte)(49)))));
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Location = new System.Drawing.Point(0, 0);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(510, 22);
+            this.button4.TabIndex = 4;
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // butonAjutor
+            // 
+            this.butonAjutor.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.butonAjutor.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(49)))), ((int)(((byte)(49)))));
+            this.butonAjutor.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.butonAjutor.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.butonAjutor.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.butonAjutor.ForeColor = System.Drawing.Color.LightSeaGreen;
+            this.butonAjutor.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.butonAjutor.Location = new System.Drawing.Point(509, 0);
+            this.butonAjutor.Name = "butonAjutor";
+            this.butonAjutor.Size = new System.Drawing.Size(73, 22);
+            this.butonAjutor.TabIndex = 5;
+            this.butonAjutor.Text = "?";
+            this.butonAjutor.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.butonAjutor.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.butonAjutor.UseMnemonic = false;
+            this.butonAjutor.UseVisualStyleBackColor = false;
+            this.butonAjutor.Click += new System.EventHandler(this.butonAjutor_Click);
+            // 
+            // butonOptiuni
+            // 
+            this.butonOptiuni.AccessibleRole = System.Windows.Forms.AccessibleRole.ScrollBar;
+            this.butonOptiuni.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.butonOptiuni.AutoSize = true;
+            this.butonOptiuni.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(49)))), ((int)(((byte)(49)))));
+            this.butonOptiuni.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.butonOptiuni.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.butonOptiuni.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.butonOptiuni.ForeColor = System.Drawing.Color.LightSeaGreen;
+            this.butonOptiuni.Location = new System.Drawing.Point(302, 326);
+            this.butonOptiuni.Margin = new System.Windows.Forms.Padding(0);
+            this.butonOptiuni.Name = "butonOptiuni";
+            this.butonOptiuni.Size = new System.Drawing.Size(202, 38);
+            this.butonOptiuni.TabIndex = 6;
+            this.butonOptiuni.Text = "Optiuni";
+            this.butonOptiuni.UseVisualStyleBackColor = false;
+            this.butonOptiuni.Click += new System.EventHandler(this.butonOptiuni_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.butonOptiuni);
+            this.Controls.Add(this.butonAjutor);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.butonMinimizare);
+            this.Controls.Add(this.butonMaximizare);
+            this.Controls.Add(this.butonX);
+            this.Controls.Add(this.Iesire);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Iesire;
+        private System.Windows.Forms.Button butonX;
+        private System.Windows.Forms.Button butonMaximizare;
+        private System.Windows.Forms.Button butonMinimizare;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button butonAjutor;
+        private System.Windows.Forms.Button butonOptiuni;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace infoEducatie
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Iesire_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void butonX_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void butonMaximizare_Click(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Normal)
+            {
+                WindowState = FormWindowState.Maximized;
+            }
+            else
+                WindowState = FormWindowState.Normal;
+        }
+
+        private void butonMinimizare_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Escape)   
+                this.Close();
+        }
+
+        private void butonAjutor_Click(object sender, EventArgs e)
+        {
+            Ajutor meniuAjutor = new Ajutor();
+            meniuAjutor.Show();
+            meniuAjutor.Focus();
+            
+            butonAjutor.Click-=butonAjutor_Click;
+        }
+
+        private void butonOptiuni_Click(object sender, EventArgs e)
+        {
+            Iesire.Visible = false;
+            butonOptiuni.Visible = false;
+        }
+    }
+}
